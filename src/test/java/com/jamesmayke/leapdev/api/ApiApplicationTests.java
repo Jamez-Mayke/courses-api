7e5c563818fc9069217c7a31bb2571af4530342a
jamesmayke <jamezmayke28@gmail.com>
2024-10-15 15:13:27 -0300

    Init project leapDev

---
+++ src/test/java/com/jamesmayke/leapdev/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamesmayke.leapdev.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
